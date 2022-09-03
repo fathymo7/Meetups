9f7e14371b28aff6fc1f87ef8112f79c7e0b43ef
Fathy <fathymohsen47@gmail.com>
2022-09-03 14:12:37 +0200

    Update

--- pages/[meetupId]/index.js
+++ pages/[meetupId]/index.js
@@ -1,9 +1,10 @@
 import MeetupDetail from "../../components/meetups/MeetupDetail";
+
 function MeetupDetails() {
   return (
     <MeetupDetail
-      image="https://ar.wikipedia.org/wiki/%D8%A8%D8%B1%D8%AC_%D8%A8%D9%8A%D8%B2%D8%A7_%D8%A7%D9%84%D9%85%D8%A7%D8%A6%D9%84#/media/%D9%85%D9%84%D9%81:The_Leaning_Tower_of_Pisa_SB.jpeg"
       title="A first Meetup"
+      image="https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg"
       address="Rome"
       description="description"
     />
@@ -36,7 +37,7 @@ export async function getStaticProps(context) {
       meetUpData: {
         id: meetupId,
         image:
-          "https://ar.wikipedia.org/wiki/%D8%A8%D8%B1%D8%AC_%D8%A8%D9%8A%D8%B2%D8%A7_%D8%A7%D9%84%D9%85%D8%A7%D8%A6%D9%84#/media/%D9%85%D9%84%D9%81:The_Leaning_Tower_of_Pisa_SB.jpeg",
+          "https://upload.wikimedia.org/wikipedia/commons/thumb/d/d3/Stadtbild_M%C3%BCnchen.jpg/1280px-Stadtbild_M%C3%BCnchen.jpg",
         title: "A first Meetup",
         address: "Rome",
         description: "description",
